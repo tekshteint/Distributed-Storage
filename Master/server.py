0a16547e2d5e4ccae05171b7b718d8591ea9c815
Tom Ekshtein <tekshteint@gmail.com>
2024-04-26 17:47:25 -0700

    Cleaned and commented

--- Master/server.py
+++ Master/server.py
@@ -7,7 +7,7 @@ import subprocess
 import requests
 import socket
 
-# Get all local IPs
+#Get all local IPs to find storage nodes in docker network
 with open("/ips.txt", "w") as outfile:
     ps = subprocess.Popen(['nmap', '-sn', '172.26.0.0/24'], stdout=subprocess.PIPE)
     output = subprocess.check_output(['grep', '-Eo', '([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)'], stdin=ps.stdout, stderr=subprocess.PIPE) #using grep to filter out the IPs
@@ -20,38 +20,52 @@ localIPListPath = "/ips.txt"
 with open (localIPListPath, "r") as file:
     localIPs = [line.strip() for line in file]
 
-subprocess.run(["mkdir", "/app/uploads"])
     
 sender_ip = socket.gethostbyname(socket.gethostname())
 localIPs.remove(sender_ip) #Removing the master's own IP so we don't send the message to ourselves
 localIPs.pop(0) #Removing the first IP because it's the gateway
-print("LOCAL IPS: ", localIPs)
 
+#Flask setup
 app =   Flask(__name__) 
 UPLOAD_FOLDER = Path(__file__).parent.joinpath('uploads')
-print(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 def generateID():
+    """Generates a UUID
+
+    Returns:
+        str: A UUID
+    """
     return str(uuid.uuid4())
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    """Upload endpoint for the client
+
+    Returns:
+        json: A JSON object containing the file ID
+    """
+    #Check if the file is in the request
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
     
+    #Get the file from the request
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
     
-    fileID = generateID()
-    file.save(app.config['UPLOAD_FOLDER'] / (fileID + "_" + file.filename + ".whole"))
-    FILEPATH = app.config['UPLOAD_FOLDER'] / (fileID + "_" + file.filename + ".whole")
+    fileID = generateID() #Generate a file ID
+    file.save(app.config['UPLOAD_FOLDER'] / (fileID + "_" + file.filename + ".whole")) #Save the file
+    FILEPATH = app.config['UPLOAD_FOLDER'] / (fileID + "_" + file.filename + ".whole") #Adding the filepath to Flask's config
     
+    #Split the file into portions depending on storage node network size
     numNodes = len(localIPs)
     fileSize = os.path.getsize(FILEPATH)
     portionSize = fileSize // numNodes
     
+    #Split the file into portions
     fileSplits = []
     with open(FILEPATH, 'rb') as f:
         for i in range(numNodes):
@@ -61,13 +75,14 @@ def upload():
             data = f.read(end_pos - start_pos) if end_pos else f.read()
             fileSplits.append(data)
             
+    #Send the split files to the storage nodes
     for i in range(numNodes):
         with open(app.config['UPLOAD_FOLDER'] / (fileID + "_" + file.filename + f".{i}split"), 'wb') as f:
             f.write(fileSplits[i])
             files = {'file': open(f.name, 'rb')}
             print("Sending to: ", localIPs[i])
             r = requests.post(f'http://{localIPs[i]}:8080/upload', files=files)
-            if r == requests.codes.ok:
+            if r == requests.codes.ok: #If the file was successfully sent, remove the split file from the master node
                 os.remove(f)
             
     os.remove(FILEPATH)
@@ -80,17 +95,30 @@ def upload():
 
 @app.route('/download/<fileID>', methods=['GET'])
 def download(fileID):
+    """Download endpoint for clients to retrieve their files
+
+    Args:
+        fileID (str): The file's UUID
+
+    Returns:
+        Client's original file
+    """
+    
+    #Download the file from the storage nodes
     for i in localIPs:
         r = requests.get(f'http://{i}:8080/download/{fileID}')
     
     
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     
-    matchingFiles = [filename for filename in files if fileID in filename]
+    matchingFiles = [filename for filename in files if fileID in filename] #Find the matching files
     
+    #If the file is not found, return an error
     if len(matchingFiles) == 0:
         return jsonify({'error': 'File not found'})
     
+    
+    #Reconstruct the file from the split files
     wholeFile = str(app.config['UPLOAD_FOLDER'] / matchingFiles[0].replace(fileID + "_", ''))
     wholeFileCleaned = re.sub(r'\.[0-9]split+', '', wholeFile)
     
--- StorageNodes/storage.py
+++ StorageNodes/storage.py
@@ -1,29 +1,41 @@
 from flask import Flask, request, jsonify, send_file
 import os
-import http
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = '/uploads'  
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create the upload folder if it doesn't exist
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # Add the upload folder to the app configuration
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    """File upload endpoint used by the Master Node
+
+    Returns:
+        HTTP response code
+    """
     file = request.files['file']
 
     if file:
-        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename)) #If a file is present, return OK
         return "200"
     else:
-        return "400"
+        return "400" #Return Bad Request if no file is present
 
 
 @app.route('/download/<fileID>', methods=['GET'])
 def download(fileID):
+    """Download file endpoint
+
+    Args:
+        fileID (str): A given file's UUID which was generated when uploaded
+
+    Returns:
+        Uploaded file slice
+    """
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     
-    matchingFiles = [filename for filename in files if fileID in filename]
+    matchingFiles = [filename for filename in files if fileID in filename] #Finds matching files based on their UUID
     
     if len(matchingFiles) == 0:
         return jsonify({'error': 'File not found'})
